cadd870ca363274e9f8fd16726a9cd92bf115646
Bertrand JAN <bertrand.jan@aphilia.io>
2022-03-09 17:15:52 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { hot } from "react-hot-loader/root";
+
+import "./assets/styles/styles.scss";
+
+function App() {
+    return (
+        <>
+            <h1>Hello</h1>
+        </>
+    );
+}
+
+export default hot(App);
